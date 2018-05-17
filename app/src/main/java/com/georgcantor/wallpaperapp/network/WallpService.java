a20ed6ed9f7913555d6132e945144ceeb50cba83
GeorgCantor <cupsman1986@gmail.com>
2018-05-17 23:07:27 +0500

    add WallpService

---
+++ app/src/main/java/com/georgcantor/wallpaperapp/network/WallpService.java
@@ -0,0 +1,47 @@
+package com.georgcantor.wallpaperapp.network;
+
+import android.content.Context;
+
+public class WallpService {
+
+    private final NetworkUtilities networkUtilities;
+    private Context context;
+    private AsyncResponse output;
+    private int index;
+    private String type;
+
+    public WallpService(NetworkUtilities networkUtilities, Context context,
+                        AsyncResponse output, int index, String type) {
+        this.networkUtilities = networkUtilities;
+        this.context = context;
+        this.output = output;
+        this.index = index;
+        this.type = type;
+    }
+
+    public void loadWallp() {
+        if (networkUtilities.isInternetConnectionPresent()) {
+            switch (type) {
+                case "latest":
+                    FetchNavTask fwt = new FetchNavTask(context, output, index);
+                    fwt.execute();
+                    break;
+
+                case "popular":
+                    FetchWallpTask fnt = new FetchWallpTask(context, output, index);
+                    fnt.execute();
+                    break;
+
+                case "editors_choice":
+                    FetchEditTask fet = new FetchEditTask(context, output, index);
+                    fet.execute();
+                    break;
+
+                default:
+                    FetchCatTask fct = new FetchCatTask(context, output, index, type);
+                    fct.execute();
+                    break;
+            }
+        }
+    }
+}
