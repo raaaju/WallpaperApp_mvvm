d071272434c648bedc717bf2c846596c9f89145c
GeorgCantor <cupsman1986@gmail.com>
2018-05-18 22:36:13 +0500

    add FetchNavTask

---
+++ app/src/main/java/com/georgcantor/wallpaperapp/network/FetchNavTask.java
@@ -0,0 +1,13 @@
+package com.georgcantor.wallpaperapp.network;
+
+import android.os.AsyncTask;
+
+import com.georgcantor.wallpaperapp.model.Pic;
+
+public class FetchNavTask extends AsyncTask<Void, Void, Pic> {
+
+    @Override
+    protected Pic doInBackground(Void... voids) {
+        return null;
+    }
+}
