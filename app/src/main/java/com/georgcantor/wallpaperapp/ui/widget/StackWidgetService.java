f387d350e8750bdc3ae2b47608fc1e5745712ff8
GeorgCantor <cupsman1986@gmail.com>
2018-05-20 21:22:32 +0500

    add StackWidgetService

--- app/src/main/java/com/georgcantor/wallpaperapp/ui/widget/StackWidgetService.java
+++ app/src/main/java/com/georgcantor/wallpaperapp/ui/widget/StackWidgetService.java
@@ -1,12 +1,78 @@
 package com.georgcantor.wallpaperapp.ui.widget;
 
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import com.georgcantor.wallpaperapp.model.Hit;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StackWidgetService extends RemoteViewsService {
 
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return null;
+        return new StackRemoteViewsFactory(this.getApplicationContext(), intent);
+    }
+
+    class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+        private List<Hit> mWidgetItems = new ArrayList<>();
+        private Context mContext;
+        private int mAppWidgetId;
+
+        public StackRemoteViewsFactory(Context context, Intent intent) {
+            mContext = context;
+            mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        @Override
+        public void onCreate() {
+
+        }
+
+        @Override
+        public void onDataSetChanged() {
+
+        }
+
+        @Override
+        public void onDestroy() {
+
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            return null;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 0;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
     }
 }
