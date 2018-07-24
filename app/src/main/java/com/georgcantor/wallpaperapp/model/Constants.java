f5bb6139ebe0aab6fe2b41f603283daaa79c1583
GeorgCantor <cupsman1986@gmail.com>
2018-07-24 13:22:13 +0900

    some fixes

--- app/src/main/java/com/georgcantor/wallpaperapp/model/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.georgcantor.wallpaperapp.model;
-
-
-public class Constants {
-
-    public static final String BASE_URL = "http://pixabay.com/api/";
-}
--- app/src/main/java/com/georgcantor/wallpaperapp/network/ApiClient.java
+++ app/src/main/java/com/georgcantor/wallpaperapp/network/ApiClient.java
@@ -1,7 +1,5 @@
 package com.georgcantor.wallpaperapp.network;
 
-import com.georgcantor.wallpaperapp.model.Constants;
-
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -9,12 +7,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
+    private static final String BASE_URL = "http://pixabay.com/api/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(OkHttpClient.Builder httpClient) {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.BASE_URL)
+                    .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(httpClient.build())
                     .build();
