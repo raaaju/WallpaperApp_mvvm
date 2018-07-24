34bb0f531c5e73ae66b960c1758bbb34ece1d24b
GeorgCantor <cupsman1986@gmail.com>
2018-07-24 16:42:42 +0900

    fix aboutApp

--- app/src/main/java/com/georgcantor/wallpaperapp/ui/AboutActivity.java
+++ app/src/main/java/com/georgcantor/wallpaperapp/ui/AboutActivity.java
@@ -1,48 +1,133 @@
 package com.georgcantor.wallpaperapp.ui;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.georgcantor.wallpaperapp.R;
 
 public class AboutActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_layout);
-        Toolbar toolbar = findViewById(R.id.toolbar_about);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle(R.string.about_us);
+        setContentView(R.layout.activity_about);
 
-        ImageView iv = findViewById(R.id.aboutimage);
-        iv.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = findViewById(R.id.toolbar_layout_about);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = findViewById(R.id.fab_about);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources()
-                        .getString(R.string.pixabay_link))));
+            public void onClick(View view) {
+                sendEmail();
             }
         });
+
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar_about);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    isShow = true;
+                } else if (isShow) {
+                    isShow = false;
+                }
+            }
+        });
+
+        AssetManager assetManager = this.getApplicationContext().getAssets();
+        Typeface montserrat_regular =
+                Typeface.createFromAsset(assetManager, "fonts/Montserrat-Regular.ttf");
+
+        TextView aboutHeaderAppDescription = findViewById(R.id.about_header_app_description);
+        aboutHeaderAppDescription.setTypeface(montserrat_regular);
+
+        TextView cardInfo = findViewById(R.id.tv_card_info);
+        cardInfo.setTypeface(montserrat_regular);
+
+        TextView madeWithLove = findViewById(R.id.tv_made_with_love);
+        madeWithLove.setTypeface(montserrat_regular);
+
+        TextView tryMyApp = findViewById(R.id.tv_try_my_new_app);
+        tryMyApp.setTypeface(montserrat_regular);
+
+        CardView cardViewInfo = findViewById(R.id.card_info);
+        CardView cardViewNewApp = findViewById(R.id.card_new_app);
+
+        cardViewInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/dev?id=5242637664196553916&hl=en"));
+                startActivity(browserIntent);
+            }
+        });
+
+        cardViewNewApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=com.georgcantor.vipnews&hl=en"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void sendEmail() {
+        Log.i("Send email", "");
+        String[] TO = {"cupsman1986@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(AboutActivity.this,
+                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
