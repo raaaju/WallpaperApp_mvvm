607757d458ce20f3eb2d80a82637fb7e54dc98c9
GeorgCantor <cupsman1986@gmail.com>
2018-11-09 19:54:53 +0700

    main: refactor SelectCatActivity to Kotlin

---
+++ app/src/main/java/com/georgcantor/wallpaperapp/ui/SelectCatActivity.kt
@@ -0,0 +1,137 @@
+package com.georgcantor.wallpaperapp.ui
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import com.georgcantor.wallpaperapp.R
+import com.georgcantor.wallpaperapp.model.Pic
+import com.georgcantor.wallpaperapp.network.ApiClient
+import com.georgcantor.wallpaperapp.network.ApiService
+import com.georgcantor.wallpaperapp.network.NetworkUtilities
+import com.georgcantor.wallpaperapp.network.interceptors.OfflineResponseCacheInterceptor
+import com.georgcantor.wallpaperapp.network.interceptors.ResponseCacheInterceptor
+import com.georgcantor.wallpaperapp.ui.adapter.WallpAdapter
+import com.georgcantor.wallpaperapp.ui.util.EndlessRecyclerViewScrollListener
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.File
+import java.util.concurrent.TimeUnit
+
+class SelectCatActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_CAT = "category"
+    }
+
+    lateinit var catAdapter: WallpAdapter
+    private lateinit var recyclerViewCat: RecyclerView
+    private lateinit var networkUtilities: NetworkUtilities
+    private var type: String? = null
+    var columnNo: Int = 0
+
+    private var picResult: Pic? = Pic()
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        networkUtilities = NetworkUtilities(this)
+        type = intent.getStringExtra(EXTRA_CAT)
+        setContentView(R.layout.activity_select_category)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_category)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.title = type
+        loadNextDataFromApi(1)
+        recyclerViewCat = findViewById(R.id.SelCatRecView)
+        recyclerViewCat.setHasFixedSize(true)
+
+        checkScreenSize()
+        val staggeredGridLayoutManager = StaggeredGridLayoutManager(columnNo,
+                StaggeredGridLayoutManager.VERTICAL)
+        recyclerViewCat.layoutManager = staggeredGridLayoutManager
+
+        val listener = object : EndlessRecyclerViewScrollListener(staggeredGridLayoutManager) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
+                loadNextDataFromApi(page)
+            }
+        }
+        recyclerViewCat.addOnScrollListener(listener)
+        catAdapter = WallpAdapter(this)
+        recyclerViewCat.adapter = catAdapter
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.pull_in_left, R.anim.push_out_right)
+    }
+
+    fun loadNextDataFromApi(index: Int) {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addNetworkInterceptor(ResponseCacheInterceptor())
+        httpClient.addInterceptor(OfflineResponseCacheInterceptor())
+        httpClient.cache(Cache(File(this@SelectCatActivity
+                .cacheDir, "ResponsesCache"), (10 * 1024 * 1024).toLong()))
+        httpClient.readTimeout(60, TimeUnit.SECONDS)
+        httpClient.connectTimeout(60, TimeUnit.SECONDS)
+        httpClient.addInterceptor(logging)
+
+        val client = ApiClient.getClient(httpClient).create(ApiService::class.java)
+        val call: Call<Pic>
+        call = client.getCatPic(type, index)
+        call.enqueue(object : Callback<Pic> {
+            override fun onResponse(call: Call<Pic>, response: Response<Pic>) {
+                try {
+                    if (!response.isSuccessful) {
+                        Log.d(resources.getString(R.string.No_Success),
+                                response.errorBody()!!.string())
+                    } else {
+                        picResult = response.body()
+                        if (picResult != null) {
+                            catAdapter.setPicList(picResult)
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onFailure(call: Call<Pic>, t: Throwable) {
+                Toast.makeText(this@SelectCatActivity, resources
+                        .getString(R.string.wrong_message), Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            overridePendingTransition(R.anim.pull_in_left, R.anim.push_out_right)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun checkScreenSize() {
+        val screenSize = resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK
+
+        columnNo = when (screenSize) {
+            Configuration.SCREENLAYOUT_SIZE_XLARGE -> 4
+            Configuration.SCREENLAYOUT_SIZE_UNDEFINED -> 3
+            Configuration.SCREENLAYOUT_SIZE_LARGE -> 3
+            Configuration.SCREENLAYOUT_SIZE_NORMAL -> 2
+            Configuration.SCREENLAYOUT_SIZE_SMALL -> 2
+            else -> 2
+        }
+    }
+}
