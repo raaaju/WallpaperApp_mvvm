8e9f1da3ec2273625ca203e2d4233c7923662244
GeorgCantor <cupsman1986@gmail.com>
2019-02-24 13:45:35 +0700

    added bottomNavigation

--- app/src/main/java/com/georgcantor/wallpaperapp/ui/MainActivity.kt
+++ app/src/main/java/com/georgcantor/wallpaperapp/ui/MainActivity.kt
@@ -5,11 +5,11 @@ import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.NavigationView
 import android.support.design.widget.Snackbar
-import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
-import android.support.v4.view.ViewPager
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -19,8 +19,9 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
-import com.georgcantor.wallpaperapp.R
-import com.georgcantor.wallpaperapp.ui.adapter.PagerAdapter
+import com.georgcantor.wallpaperapp.ui.fragment.BmwFragment
+import com.georgcantor.wallpaperapp.ui.fragment.CategoryFragment
+import com.georgcantor.wallpaperapp.ui.fragment.MercedesFragment
 import com.georgcantor.wallpaperapp.ui.util.UtilityMethods
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -29,14 +30,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setContentView(com.georgcantor.wallpaperapp.R.layout.activity_main)
+        val drawer = findViewById<DrawerLayout>(com.georgcantor.wallpaperapp.R.id.drawer_layout)
+        val toolbar = findViewById<Toolbar>(com.georgcantor.wallpaperapp.R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        supportFragmentManager.beginTransaction()
+                .replace(com.georgcantor.wallpaperapp.R.id.frame_container, MercedesFragment())
+                .commit()
 
         if (!UtilityMethods.isNetworkAvailable()) {
             Snackbar.make(findViewById<View>(android.R.id.content),
-                    resources.getString(R.string.check_internet), Snackbar.LENGTH_LONG).show()
+                    resources.getString(com.georgcantor.wallpaperapp.R.string.check_internet), Snackbar.LENGTH_LONG).show()
         }
         val myTitle = toolbar.getChildAt(0) as TextView
 
@@ -47,18 +52,47 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             myTitle.setTypeface(typeface, Typeface.BOLD)
         }
         val toggle = ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+                com.georgcantor.wallpaperapp.R.string.navigation_drawer_open, com.georgcantor.wallpaperapp.R.string.navigation_drawer_close)
         drawer.setDrawerListener(toggle)
         toggle.syncState()
 
-        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                com.georgcantor.wallpaperapp.R.id.navigation_shop -> {
+                    toolbar.title = "Mercedes-Benz"
+                    val fragment = MercedesFragment.newInstance()
+                    openFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                com.georgcantor.wallpaperapp.R.id.navigation_gifts -> {
+                    toolbar.title = "BMW"
+                    val fragment = BmwFragment.newInstance()
+                    openFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                com.georgcantor.wallpaperapp.R.id.navigation_cart -> {
+                    toolbar.title = "Gallery"
+                    val fragment = CategoryFragment.newInstance()
+                    openFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
+        val navigation = findViewById<View>(com.georgcantor.wallpaperapp.R.id.navigation) as BottomNavigationView
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val navigationView = findViewById<NavigationView>(com.georgcantor.wallpaperapp.R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
         navigationView.itemIconTintList = null
-        val viewPager = findViewById<ViewPager>(R.id.viewPager)
-        viewPager.adapter = PagerAdapter(supportFragmentManager)
-        viewPager.offscreenPageLimit = 3
-        val tabLayout = findViewById<TabLayout>(R.id.sliding_tabs)
-        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(com.georgcantor.wallpaperapp.R.id.frame_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
     public override fun onStart() {
@@ -67,7 +101,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
         if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.KITKAT) {
-            val searchItem = menu?.findItem(R.id.action_search)
+            val searchItem = menu?.findItem(com.georgcantor.wallpaperapp.R.id.action_search)
             searchItem?.isVisible = false
         }
 
@@ -75,14 +109,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(com.georgcantor.wallpaperapp.R.menu.menu_main, menu)
 
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.action_search -> {
+            com.georgcantor.wallpaperapp.R.id.action_search -> {
                 openSearchActivity()
             }
         }
@@ -92,100 +126,104 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     private fun openSearchActivity() {
         startActivity(Intent(this, SearchActivity::class.java))
-        overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+        overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_aston -> {
+            com.georgcantor.wallpaperapp.R.id.nav_aston -> {
                 val intentAston = Intent(this, FetchActivity::class.java)
                 intentAston.putExtra(FetchActivity.FETCH_TYPE,
-                        resources.getString(R.string.aston_walp))
+                        resources.getString(com.georgcantor.wallpaperapp.R.string.aston_walp))
                 this.startActivity(intentAston)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_bentley -> {
+            com.georgcantor.wallpaperapp.R.id.nav_bentley -> {
                 val intentBentley = Intent(this, FetchActivity::class.java)
                 intentBentley.putExtra(FetchActivity.FETCH_TYPE,
-                        resources.getString(R.string.bentley_walp))
+                        resources.getString(com.georgcantor.wallpaperapp.R.string.bentley_walp))
                 this.startActivity(intentBentley)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_porsche -> {
+            com.georgcantor.wallpaperapp.R.id.nav_porsche -> {
                 val intentPorsche = Intent(this, FetchActivity::class.java)
                 intentPorsche.putExtra(FetchActivity.FETCH_TYPE,
-                        resources.getString(R.string.porsche))
+                        resources.getString(com.georgcantor.wallpaperapp.R.string.porsche))
                 this.startActivity(intentPorsche)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_audi -> {
+            com.georgcantor.wallpaperapp.R.id.nav_audi -> {
                 val intentAudi = Intent(this, FetchActivity::class.java)
                 intentAudi.putExtra(FetchActivity.FETCH_TYPE,
-                        resources.getString(R.string.audi))
+                        resources.getString(com.georgcantor.wallpaperapp.R.string.audi))
                 this.startActivity(intentAudi)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_bugatti -> {
+            com.georgcantor.wallpaperapp.R.id.nav_bugatti -> {
                 val intentBugatti = Intent(this, FetchActivity::class.java)
                 intentBugatti.putExtra(FetchActivity.FETCH_TYPE,
-                        resources.getString(R.string.bugatti))
+                        resources.getString(com.georgcantor.wallpaperapp.R.string.bugatti))
                 this.startActivity(intentBugatti)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_mclaren -> {
+            com.georgcantor.wallpaperapp.R.id.nav_mclaren -> {
                 val intentMclaren = Intent(this, FetchActivity::class.java)
                 intentMclaren.putExtra(FetchActivity.FETCH_TYPE,
-                        resources.getString(R.string.mclaren_walp))
+                        resources.getString(com.georgcantor.wallpaperapp.R.string.mclaren_walp))
                 this.startActivity(intentMclaren)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_ferrari -> {
+            com.georgcantor.wallpaperapp.R.id.nav_ferrari -> {
                 val intentFerrari = Intent(this, FetchActivity::class.java)
                 intentFerrari.putExtra(FetchActivity.FETCH_TYPE,
-                        resources.getString(R.string.ferrari))
+                        resources.getString(com.georgcantor.wallpaperapp.R.string.ferrari))
                 this.startActivity(intentFerrari)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_lambo -> {
+            com.georgcantor.wallpaperapp.R.id.nav_lambo -> {
                 val intentLambo = Intent(this, FetchActivity::class.java)
                 intentLambo.putExtra(FetchActivity.FETCH_TYPE,
-                        resources.getString(R.string.lamborghini))
+                        resources.getString(com.georgcantor.wallpaperapp.R.string.lamborghini))
                 this.startActivity(intentLambo)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_favorites -> {
+            com.georgcantor.wallpaperapp.R.id.nav_favorites -> {
                 startActivity(Intent(this@MainActivity, FavoriteActivity::class.java))
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_about -> {
+            com.georgcantor.wallpaperapp.R.id.nav_about -> {
                 val intentAbout = Intent(this, AboutActivity::class.java)
                 this.startActivity(intentAbout)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
-            R.id.nav_license -> {
+            com.georgcantor.wallpaperapp.R.id.nav_license -> {
                 val intentLicense = Intent(this, LicenseActivity::class.java)
                 this.startActivity(intentLicense)
-                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+                overridePendingTransition(com.georgcantor.wallpaperapp.R.anim.pull_in_right, com.georgcantor.wallpaperapp.R.anim.push_out_left)
             }
         }
-        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val drawer = findViewById<DrawerLayout>(com.georgcantor.wallpaperapp.R.id.drawer_layout)
         drawer.closeDrawer(GravityCompat.START)
 
         return true
     }
 
     override fun onBackPressed() {
-        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val drawer = findViewById<DrawerLayout>(com.georgcantor.wallpaperapp.R.id.drawer_layout)
         when {
             drawer.isDrawerOpen(GravityCompat.START) -> drawer.closeDrawer(GravityCompat.START)
-            doubleTap -> super.onBackPressed()
+            doubleTap -> closeApp()
             else -> {
-                Toast.makeText(this, this.resources.getString(R.string.press_back),
+                Toast.makeText(this, this.resources.getString(com.georgcantor.wallpaperapp.R.string.press_back),
                         Toast.LENGTH_SHORT).show()
                 doubleTap = true
                 val handler = Handler()
                 handler.postDelayed({ doubleTap = false }, 2000)
             }
         }
+    }
+
+    private fun closeApp() {
+        finish()
     }
 }
