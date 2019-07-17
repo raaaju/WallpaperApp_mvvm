b6b5c085f72f18d56483361d2131581ca076c3a3
GeorgCantor <cupsman1986@gmail.com>
2019-07-17 13:09:08 +0300

    moved EndlessRecyclerViewScrollListener to Kotlin

---
+++ app/src/main/java/com/georgcantor/wallpaperapp/ui/util/EndlessRecyclerViewScrollListener.kt
@@ -0,0 +1,78 @@
+package com.georgcantor.wallpaperapp.ui.util
+
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+
+abstract class EndlessRecyclerViewScrollListener(layoutManager: StaggeredGridLayoutManager) : RecyclerView.OnScrollListener() {
+
+    private var visibleThreshold = 10
+    private var currentPage = 1
+    private var previousTotalItemCount = 0
+    private var loading = true
+    private val startingPageIndex = 1
+
+    private var manager: RecyclerView.LayoutManager = layoutManager
+
+    init {
+        visibleThreshold *= layoutManager.spanCount
+    }
+
+    private fun getLastVisibleItem(lastVisibleItemPositions: IntArray): Int {
+        var maxSize = 0
+        for (i in lastVisibleItemPositions.indices) {
+            if (i == 0) {
+                maxSize = lastVisibleItemPositions[i]
+            } else if (lastVisibleItemPositions[i] > maxSize) {
+                maxSize = lastVisibleItemPositions[i]
+            }
+        }
+
+        return maxSize
+    }
+
+    override fun onScrolled(view: RecyclerView, dx: Int, dy: Int) {
+        var lastVisibleItemPosition = 0
+        val totalItemCount = manager.itemCount
+
+        when (manager) {
+            is StaggeredGridLayoutManager -> {
+                val lastVisibleItemPositions = (manager as StaggeredGridLayoutManager)
+                        .findLastVisibleItemPositions(null)
+                lastVisibleItemPosition = getLastVisibleItem(lastVisibleItemPositions)
+            }
+            is GridLayoutManager -> lastVisibleItemPosition = (manager as GridLayoutManager)
+                    .findLastVisibleItemPosition()
+            is LinearLayoutManager -> lastVisibleItemPosition = (manager as LinearLayoutManager)
+                    .findLastVisibleItemPosition()
+        }
+
+        if (totalItemCount < previousTotalItemCount) {
+            this.currentPage = this.startingPageIndex
+            this.previousTotalItemCount = totalItemCount
+            if (totalItemCount == 0) {
+                this.loading = true
+            }
+        }
+
+        if (loading && totalItemCount > previousTotalItemCount) {
+            loading = false
+            previousTotalItemCount = totalItemCount
+        }
+
+        if (!loading && lastVisibleItemPosition + visibleThreshold > totalItemCount) {
+            currentPage++
+            onLoadMore(currentPage, totalItemCount, view)
+            loading = true
+        }
+    }
+
+    fun resetState() {
+        this.currentPage = this.startingPageIndex
+        this.previousTotalItemCount = 0
+        this.loading = true
+    }
+
+    abstract fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView)
+}
