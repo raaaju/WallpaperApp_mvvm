7eb6d777c0fce762fe52d69a0a1856a182852868
GeorgCantor <cupsman1986@gmail.com>
2019-09-17 19:09:44 +0300

    added caching in okhttp

--- app/src/main/java/com/georgcantor/wallpaperapp/model/remote/ApiClient.kt
+++ app/src/main/java/com/georgcantor/wallpaperapp/model/remote/ApiClient.kt
@@ -1,11 +1,14 @@
 package com.georgcantor.wallpaperapp.model.remote
 
 import com.georgcantor.wallpaperapp.BuildConfig
+import com.georgcantor.wallpaperapp.MyApplication
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.concurrent.TimeUnit
 
 object ApiClient {
@@ -20,10 +23,14 @@ object ApiClient {
         }
 
         val okHttpClient = OkHttpClient().newBuilder()
-            .connectTimeout(20, TimeUnit.SECONDS)
-            .readTimeout(20, TimeUnit.SECONDS)
-            .writeTimeout(20, TimeUnit.SECONDS)
-            .build()
+                .addNetworkInterceptor(ResponseCacheInterceptor())
+                .addInterceptor(OfflineResponseCacheInterceptor())
+                .cache(Cache(File(MyApplication.instance?.cacheDir,
+                        "ResponsesCache"), (10 * 1024 * 1024).toLong()))
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .build()
 
         retrofit = Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
