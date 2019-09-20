da545bcfdb5af37828f9c02800a8daab624793b7
alexandr.kirillov <alexandr.kirillov@osinit.com>
2019-09-20 14:15:36 +0300

    added Firebase

--- app/src/main/java/com/georgcantor/wallpaperapp/model/remote/ApiService.kt
+++ app/src/main/java/com/georgcantor/wallpaperapp/model/remote/ApiService.kt
@@ -11,4 +11,6 @@ interface ApiService {
     @GET("?key=" + BuildConfig.API_KEY)
     fun getPictures(@Query("q") query: String,
                     @Query("page") index: Int): Observable<Pic>
+
+    //563492ad6f917000010000012802dcdebfc2451ab06a0cdf7c62fe16
 }
