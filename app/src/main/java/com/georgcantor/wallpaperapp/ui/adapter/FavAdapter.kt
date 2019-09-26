f9b7726f9508f1c3d97c375bcc4e4a3e012f9b24
alexandr.kirillov <alexandr.kirillov@osinit.com>
2019-09-26 17:31:00 +0300

    changed FavAdapter

--- app/src/main/java/com/georgcantor/wallpaperapp/ui/adapter/FavAdapter.kt
+++ app/src/main/java/com/georgcantor/wallpaperapp/ui/adapter/FavAdapter.kt
@@ -1,14 +1,20 @@
 package com.georgcantor.wallpaperapp.ui.adapter
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.RelativeLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.georgcantor.wallpaperapp.R
 import com.georgcantor.wallpaperapp.model.CommonPic
+import com.georgcantor.wallpaperapp.model.local.db.DatabaseHelper
 import com.georgcantor.wallpaperapp.model.local.db.Favorite
+import com.georgcantor.wallpaperapp.ui.PicDetailActivity
 import com.georgcantor.wallpaperapp.ui.adapter.holder.FavoriteViewHolder
+import com.georgcantor.wallpaperapp.ui.util.longToast
+import com.georgcantor.wallpaperapp.ui.util.showDialog
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import java.util.ArrayList
@@ -16,6 +22,8 @@ import java.util.ArrayList
 class FavAdapter(private val context: Context) : RecyclerView.Adapter<FavoriteViewHolder>() {
 
     private val favorites: MutableList<Favorite>?
+    private val activity = context as Activity
+    private val db = DatabaseHelper(context)
 
     init {
         this.favorites = ArrayList()
@@ -26,27 +34,56 @@ class FavAdapter(private val context: Context) : RecyclerView.Adapter<FavoriteVi
         notifyDataSetChanged()
     }
 
-    fun clearFavList() {
-        val size = favorites?.size
-        favorites?.clear()
-        size?.let { notifyItemRangeRemoved(0, it) }
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
         val itemView = LayoutInflater.from(context).inflate(R.layout.favorite_item, null)
-        val wallpViewHolder = FavoriteViewHolder(itemView)
 
-        return wallpViewHolder
+        return FavoriteViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+        val favorite = favorites?.get(position)
+        val hitJson = favorite?.hit
+
+        val gson = Gson()
+        val pic = gson.fromJson(hitJson, CommonPic::class.java)
+
+        holder.imageView.setOnClickListener {
+            val intent = Intent(context, PicDetailActivity::class.java)
+            try {
+                intent.putExtra(
+                    PicDetailActivity.EXTRA_PIC,
+                    CommonPic(
+                        url = pic.url,
+                        width = pic.width,
+                        heght = pic.heght,
+                        likes = pic.likes,
+                        favorites = pic.favorites,
+                        tags = pic.tags,
+                        downloads = pic.downloads,
+                        imageURL = pic.imageURL,
+                        fullHDURL = pic.fullHDURL,
+                        user = pic.user,
+                        userImageURL = pic.userImageURL
+                    )
+                )
+            } catch (e: ArrayIndexOutOfBoundsException) {
+                context.longToast(context.resources.getString(R.string.something_went_wrong))
+            }
+            context.startActivity(intent)
+            activity.overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left)
+        }
+
+        holder.imageView.setOnLongClickListener {
+            val photo = favorites?.get(position)
+            val url = photo?.imageUrl
+
+            context.showDialog(context.resources.getString(R.string.del_from_fav_dialog)) {
+                deleteFromFavorites(url)
+            }
+            false
+        }
+
         this.favorites.let {
-            val favorite = favorites?.get(position)
-            val hitJson = favorite?.hit
-
-            val gson = Gson()
-            val pic = gson.fromJson(hitJson, CommonPic::class.java)
-
             val layoutParams = holder.imageView.layoutParams as RelativeLayout.LayoutParams
             val height = pic.heght.toFloat()
             val width = pic.width.toFloat()
@@ -63,5 +100,12 @@ class FavAdapter(private val context: Context) : RecyclerView.Adapter<FavoriteVi
     }
 
     override fun getItemCount(): Int = favorites?.size ?: 0
+
+    private fun deleteFromFavorites(url: String?) {
+        if (url != null) {
+            db.deleteFromFavorites(url)
+        }
+        activity.recreate()
+    }
 
 }
