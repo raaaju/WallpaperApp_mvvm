f58ed177c0f99b545294d20ffd5f2806517f25fd
Anton Machikhin <anton.machikhin@osinit.com>
2019-11-08 13:40:27 +0300

    some fixes

--- app/src/main/java/com/georgcantor/wallpaperapp/util/Extensions.kt
+++ app/src/main/java/com/georgcantor/wallpaperapp/util/Extensions.kt
@@ -32,12 +32,22 @@ fun AppCompatActivity.openFragment(fragment: Fragment, tag: String, mustRemove: 
     if (fragment == current && fragment !is CarBrandFragment) {
         return
     } else if (fragment.isAdded) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) transaction.setCustomAnimations(R.anim.pull_in_right, R.anim.push_out_left, R.anim.pull_in_left, R.anim.push_out_right)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) transaction.setCustomAnimations(
+            R.anim.pull_in_right,
+            R.anim.push_out_left,
+            R.anim.pull_in_left,
+            R.anim.push_out_right
+        )
         transaction.replace(R.id.frame_container, fragment)
         transaction.addToBackStack(tag)
         transaction.commit()
     } else {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) transaction.setCustomAnimations(R.anim.pull_in_right, R.anim.push_out_left, R.anim.pull_in_left, R.anim.push_out_right)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) transaction.setCustomAnimations(
+            R.anim.pull_in_right,
+            R.anim.push_out_left,
+            R.anim.pull_in_left,
+            R.anim.push_out_right
+        )
         transaction.add(R.id.frame_container, fragment)
         transaction.addToBackStack(tag)
         transaction.commit()
