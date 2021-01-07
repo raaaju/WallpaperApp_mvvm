f81bc847af25e1ea3bacd26d1f8e28536f369b5a
GeorgCantor <cupsman1986@gmail.com>
2021-01-07 20:16:32 +0300

    added DrawerLayout with menu

--- app/src/main/java/com/georgcantor/wallpaperapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/georgcantor/wallpaperapp/ui/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,8 +16,9 @@ import com.georgcantor.wallpaperapp.util.setVisibility
 import com.georgcantor.wallpaperapp.util.setupWithNavController
 import com.georgcantor.wallpaperapp.util.startActivity
 import com.georgcantor.wallpaperapp.util.viewBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private val binding by viewBinding(ActivityMainBinding::inflate)
     private var currentNavController: LiveData<NavController>? = null
@@ -25,6 +27,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         if (savedInstanceState == null) setupBottomNavigationBar()
+
+        binding.navView.setNavigationItemSelectedListener(this)
+        binding.navView.itemIconTintList = null
 
         getNetworkLiveData(applicationContext).observe(this) {
             binding.noInternetWarning.setVisibility(!it)
@@ -67,5 +72,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return currentNavController?.value?.navigateUp() ?: false
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_aston -> {}
+            R.id.nav_bentley -> {}
+            R.id.nav_bugatti -> {}
+            R.id.nav_ferrari -> {}
+            R.id.nav_lambo -> {}
+            R.id.nav_mclaren -> {}
+            R.id.nav_porsche -> {}
+            R.id.nav_rolls -> {}
+            R.id.nav_favorites-> {}
+            R.id.nav_rate_us -> {}
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
