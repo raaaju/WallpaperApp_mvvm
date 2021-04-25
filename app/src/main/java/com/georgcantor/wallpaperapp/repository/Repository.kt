3f33f020071d97c995a77c9a197a85bf23da5246
GeorgCantor <cupsman1986@gmail.com>
2021-04-25 16:22:48 +0300

    Clean up some Repository code

--- app/src/main/java/com/georgcantor/wallpaperapp/repository/Repository.kt
+++ app/src/main/java/com/georgcantor/wallpaperapp/repository/Repository.kt
@@ -7,51 +7,52 @@ import com.georgcantor.wallpaperapp.model.local.Favorite
 import com.georgcantor.wallpaperapp.model.remote.ApiService
 import com.georgcantor.wallpaperapp.model.remote.response.CommonPic
 import com.georgcantor.wallpaperapp.util.Constants.VIDEOS
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class Repository(
     private val service: ApiService,
     private val dao: FavDao
 ) {
 
-    suspend fun getPictures(
-        query: String,
-        page: Int
-    ): List<CommonPic> {
-
-        val respUnsplash = service.getUnsplashPictures(UNSPLASH_URL, query, page)
-        val respPixabay = service.getPixabayPictures(query, page)
-
-        val listUnsplash = respUnsplash.body()?.results?.map {
-            CommonPic(
-                url = it.urls?.small ?: "",
-                width = it.width ?: 0,
-                height = it.height ?: 0,
-                tags = it.altDescription,
-                imageURL = it.urls?.full,
-                fullHDURL = it.urls?.regular,
-                id = it.hashCode(),
-                videoId = ""
-            )
-        }
-
-        val listPixabay = respPixabay.body()?.hits?.map {
-            CommonPic(
-                url = it.webformatURL ?: "",
-                width = it.imageWidth,
-                height = it.imageHeight,
-                tags = it.tags,
-                imageURL = it.imageURL,
-                fullHDURL = it.webformatURL,
-                id = it.id,
-                videoId = ""
-            )
-        }
-
+    suspend fun getPictures(query: String, page: Int): List<CommonPic> {
         val list = mutableListOf<CommonPic>()
-        listUnsplash?.let { list.addAll(it) }
-        listPixabay?.let { it.forEach { if (it.id != 158703 && it.id != 158704) list.add(it) } }
 
-        return list.shuffled()
+        withContext(Dispatchers.IO) {
+            val respUnsplash = service.getUnsplashPictures(UNSPLASH_URL, query, page)
+            val listUnsplash = respUnsplash.body()?.results?.map {
+                CommonPic(
+                    url = it.urls?.small ?: "",
+                    width = it.width ?: 0,
+                    height = it.height ?: 0,
+                    tags = it.altDescription,
+                    imageURL = it.urls?.full,
+                    fullHDURL = it.urls?.regular,
+                    id = it.hashCode(),
+                    videoId = ""
+                )
+            }
+            listUnsplash?.let { list.addAll(it) }
+        }
+
+        withContext(Dispatchers.IO) {
+            val respPixabay = service.getPixabayPictures(query, page)
+            val listPixabay = respPixabay.body()?.hits?.map {
+                CommonPic(
+                    url = it.webformatURL ?: "",
+                    width = it.imageWidth,
+                    height = it.imageHeight,
+                    tags = it.tags,
+                    imageURL = it.imageURL,
+                    fullHDURL = it.webformatURL,
+                    id = it.id,
+                    videoId = ""
+                )
+            }
+            listPixabay?.let { list.addAll(it) }
+        }
+
+        return list
     }
 
     suspend fun getVideos() = service.getVideos(YOUTUBE_URL, VIDEOS)
